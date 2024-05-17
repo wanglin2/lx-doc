ab042c7e05709021cb547b4d342a6276208e2564
街角小林 <1013335014@qq.com>
2024-05-17 09:32:49 +0800

    update

--- bpmn/vite.config.js
+++ bpmn/vite.config.js
@@ -3,9 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'development' ? '/' : './',
   plugins: [vue()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }]
+  },
   server: {
-    port: 8082
+    host: '0.0.0.0',
+    port: 9098
   }
 })
