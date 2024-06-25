407119935295dfa7395ceb79fa22c887dd0a8218
街角小林 <1013335014@qq.com>
2024-06-25 19:12:06 +0800

    update

--- workbench/src/constant.js
+++ workbench/src/constant.js
@@ -7,7 +7,7 @@ export const RESOURCE_TYPES = {
 export const panoramaHelpList = [
   {
     title: '删除文件夹或文件',
-    desc: '激活文件夹或文件节点，然后按下 Del 或 Backspace 键删除'
+    desc: '激活文件夹或文件节点，然后按 Del 或 Backspace 键删除'
   },
   {
     title: '复制文件夹或文件',
@@ -15,10 +15,10 @@ export const panoramaHelpList = [
   },
   {
     title: '移动文件夹或文件',
-    desc: '鼠标按住要移动的文件夹或文件，然后拖动到要移动到的文件夹节点上松开即可'
+    desc: '鼠标按住要移动的文件夹或文件，然后拖动到要移动到的文件夹节点上松开'
   },
   {
     title: '修改文件夹或文件名称',
-    desc: '双击要要修改的文件夹或文件，然后编辑完名称后按回车键即可'
+    desc: '双击要要修改的文件夹或文件，然后编辑完名称后按回车键完成修改'
   }
 ]
